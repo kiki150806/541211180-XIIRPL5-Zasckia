07db1323583c4351dfb6457345321ffac392d133
kiki150806 <kiki.kia1908@gmail.com>
2023-11-10 19:20:25 +0700

    sixth commit

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,4 @@
-let users = [
-    {id: 1, nama: "Zasckia", email: "zasckiiaa@gmail.com"},
-    {id: 2, nama: "Nayla", email: "naylaa@gmail.com"},
-]
+const User = require('../models/User')
 
 module.exports = {
     index: (req, res) => {
